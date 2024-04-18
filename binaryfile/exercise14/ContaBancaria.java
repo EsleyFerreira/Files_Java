4a9016d0026b5e5d7f27377d0323be98e22acea9
Esley Ferreira <140576056+EsleyFerreira@users.noreply.github.com>
2024-04-17 23:27:12 -0300

    Create ContaBancaria.java

---
+++ binaryfile/exercise14/ContaBancaria.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+class ContaBancaria implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String nomeCliente;
+    private double saldo;
+
+    public ContaBancaria(String nomeCliente, double saldo) {
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaBancaria [nomeCliente=" + nomeCliente + ", saldo=" + saldo + "]";
+    }
+}
+
+public class AtualizacaoContaBancaria {
+    public static void main(String[] args) {
+        ContaBancaria conta = null;
+        try (ObjectInputStream objetoEntrada = new ObjectInputStream(new FileInputStream("home/esley/Documentos/Intellij/exercise14/conta.dat"))) {
+            conta = (ContaBancaria) objetoEntrada.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        conta.depositar(100.0);
+
+        try (ObjectOutputStream objetoSaida = new ObjectOutputStream(new FileOutputStream("home/esley/Documentos/Intellij/exercise14/conta.dat"))) {
+            objetoSaida.writeObject(conta);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
